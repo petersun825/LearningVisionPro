b2b298c81cac19d3715e2b75032608160f28cd39
petersun825 <petersun825@gmail.com>
2025-03-12 21:18:27 -0400

    comment add gestures

--- design-sprint-2025mar-template/ContentView.swift
+++ design-sprint-2025mar-template/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
                 .padding(.bottom, 50)
 
             Text("Hello, medVR!")
-
+            //add gestures
             ToggleImmersiveSpaceButton()
         }
         .padding()
