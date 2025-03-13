05d61f8a52b2fc86fc71a8a2a8ac6403dfb5e8b3
petersun825 <petersun825@gmail.com>
2025-03-12 22:03:19 -0400

    Commit Main app from Dat Tutorial

---
+++ design-sprint-2025mar-template/MainApp.swift
@@ -0,0 +1,18 @@
+//
+//  GestureControlApp.swift
+//  GestureControl
+//
+//  Created by Dat Nguyen on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        
+    }
+}
